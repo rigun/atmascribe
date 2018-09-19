d5749bf8422bfebd7dbe0b6ce1da780905023161
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-19 12:40:02 +0700

    fix catatan

--- js/CRUDcatatan.js
+++ js/CRUDcatatan.js
@@ -2,26 +2,26 @@ function getCatatanById(id){
     var penting = "";
     var lainnya = "";
 
-        $.getJSON("https://atmascribe.thekingcorp.org/api/catatan/readByUser.php?id="+id, function(catatans){
-           
-            $.each(catatans.catatan, function(key, cttn){
-                if(cttn.prioritas == 1){
-                    penting+="<tr><td><span id='cNama"+cttn.id+"'>"+cttn.catatan+"</span><span id='cRank"+cttn.id+"' style='display:none'>1</span></td>"+
-                                "<td><a  data-toggle='modal' data-target='#EditCatatan' onclick='editModalCatatan('"+cttn.id+"')'><img src='../img/icon/edit.svg' /></a>"+
-                                    "<a  data-toggle='modal' data-target='#doneCatatan' onclick='doneCatatan('"+cttn.id+"')'><img src='../img/icon/cekMini.svg' /></a></td></tr>"
-                    
-                }else if(cttn.prioritas == 0){
-                    lainnya += "<tr><td><span id='cNama"+cttn.id+"'>"+cttn.catatan+"</span><span id='cRank"+cttn.id+"' style='display:none'>0</span></td>"+
-                                "<td><a  data-toggle='modal' data-target='#EditCatatan' onclick='editModalCatatan('"+cttn.id+"')'><img src='../img/icon/edit.svg' /></a>"+
-                                "<a  data-toggle='modal' data-target='#doneCatatan' onclick='doneCatatan('"+cttn.id+"')'><img src='../img/icon/cekMini.svg' /></a></td></tr>"
-                }else{
-                    penting += "kesalahan pada server";
-                }
+    $.getJSON("https://atmascribe.thekingcorp.org/api/catatan/readByUser.php?id="+id, function(catatans){
+        
+        $.each(catatans.catatan, function(key, cttn){
+            if(cttn.prioritas == 1){
+                penting+="<tr><td><span id='cNama"+cttn.id+"'>"+cttn.catatan+"</span><span id='cRank"+cttn.id+"' style='display:none'>1</span></td>"+
+                            "<td><a  data-toggle='modal' data-target='#EditCatatan' onclick='editModalCatatan("+cttn.id+")'><img src='../img/icon/edit.svg' /></a>"+
+                                "<a  data-toggle='modal' data-target='#doneCatatan' onclick='doneCatatan("+cttn.id+")'><img src='../img/icon/cekMini.svg' /></a></td></tr>"
+                
+            }else if(cttn.prioritas == 0){
+                lainnya += "<tr><td><span id='cNama"+cttn.id+"'>"+cttn.catatan+"</span><span id='cRank"+cttn.id+"' style='display:none'>0</span></td>"+
+                            "<td><a  data-toggle='modal' data-target='#EditCatatan' onclick='editModalCatatan("+cttn.id+")'><img src='../img/icon/edit.svg' /></a>"+
+                            "<a  data-toggle='modal' data-target='#doneCatatan' onclick='doneCatatan("+cttn.id+")'><img src='../img/icon/cekMini.svg' /></a></td></tr>"
+            }else{
+                penting += "kesalahan pada server";
+            }
 
-            });               
-            $('#catatanPenting').html(penting);     
-            $('#catatanLainnya').html(lainnya);  
-        });
+        });               
+    });
+    $('#catatanPenting').html(penting);     
+    $('#catatanLainnya').html(lainnya);  
 
 }
 function createCatatan(e, id){
