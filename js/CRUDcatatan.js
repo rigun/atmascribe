b94026576e16660f4cefa1f99543fe2913a8bdc2
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-19 12:35:12 +0700

    fix catatan

--- js/CRUDcatatan.js
+++ js/CRUDcatatan.js
@@ -6,26 +6,16 @@ function getCatatanById(id){
            
             $.each(catatans.catatan, function(key, cttn){
                 if(cttn.prioritas == 1){
-                    penting += "<tr>"+
-                                    "<td><span id='cNama"+
-                                    cttn.id+
-                                    "'>"+
-                                    cttn.catatan+
-                                    "</span><span id='cRank"+
-                                    cttn.id+
-                                    "' style='display:none'>1</span></td>"+
-                                    "<td><a  data-toggle='modal' data-target='#EditCatatan' onclick='editModalCatatan('"+
-                                    cttn.id+
-                                    "')'><img src='../img/icon/edit.svg' /></a>"+
-                                    "<a  data-toggle='modal' data-target='#doneCatatan' onclick='doneCatatan('"+
-                                    cttn.id+
-                                    "')' ><img src='../img/icon/cekMini.svg' /></a></td>"+
-                                "</tr>"
+                    penting+="<tr><td><span id='cNama"+cttn.id+"'>"+cttn.catatan+"</span><span id='cRank"+cttn.id+"' style='display:none'>1</span></td>"+
+                                "<td><a  data-toggle='modal' data-target='#EditCatatan' onclick='editModalCatatan('"+cttn.id+"')'><img src='../img/icon/edit.svg' /></a>"+
+                                    "<a  data-toggle='modal' data-target='#doneCatatan' onclick='doneCatatan('"+cttn.id+"')'><img src='../img/icon/cekMini.svg' /></a></td></tr>"
+                    console.log(penting);
+                    
                 }else if(cttn.prioritas == 0){
                     lainnya += "<tr>"+
                                 "<td> <span id='cNama"+cttn.id+"'>"+cttn.catatan+"</span> <span id='cRank"+cttn.id+"' style='display:none'>0</span></td>"+
-                                "<td> <a  data-toggle='modal' data-target='#EditCatatan' onclick='editModalCatatan('"+cttn.id+"')' ><img src='../img/icon/edit.svg' /></a>"+
-                                    "<a  data-toggle='modal' data-target='#doneCatatan' onclick='doneCatatan('"+cttn.id+"')' ><img src='../img/icon/cekMini.svg' /></a></td>"+
+                                "<td> <a  data-toggle='modal' data-target='#EditCatatan' onclick='editModalCatatan("+cttn.id+")' ><img src='../img/icon/edit.svg' /></a>"+
+                                    "<a  data-toggle='modal' data-target='#doneCatatan' onclick='doneCatatan("+cttn.id+")' ><img src='../img/icon/cekMini.svg' /></a></td>"+
                                 "</tr>"
                 }else{
                     penting += "kesalahan pada server";
