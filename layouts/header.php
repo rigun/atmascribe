c3ad0947ded067c2943a41ce90ae31e6a273e055
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-19 09:54:05 +0700

    icon

--- layouts/header.php
+++ layouts/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="<?php echo BASE_URL.'/favicon.ico'; ?>">
+    <link rel="icon" href="<?php echo BASE_URL.'/favicon.png'; ?>">
     <title>Atma Scribe</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo BASE_URL.'/css/style.css'; ?>" />
