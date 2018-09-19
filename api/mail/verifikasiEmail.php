4c0b8b7022989bbc1ce59f34ed6409070a7fb6ea
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-19 10:14:59 +0700

    verif

--- api/mail/verifikasiEmail.php
+++ api/mail/verifikasiEmail.php
@@ -1,30 +1,10 @@
 <?php
 
-// include database and object files
-include_once './config/database.php';
-include_once './objects/user.php';
+
  
-// get database connection
-$database = new Database();
-$db = $database->getConnection();
-echo '{';
-    echo '"message": "Updating"';
-echo '}';
-// prepare product object
-$product = new User($db);
- 
-$product->token = isset($_GET['token']) ? $_GET['token'] : die();
+$token = isset($_GET['token']);
+echo $token ;
  
 
-if($product->updateByToken()){
-    header('Location: https://atmascribe.thekingcorp.org/successVerifikasi.php');
-    echo '{';
-        echo '"message": "Sukses Update Status."';
-    echo '}';
-}
-else{
-    echo '{';
-        echo '"message": "Gagal Update Status."';
-    echo '}';
-}
+
 ?>
