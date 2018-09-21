84d9e17a4037bcc4e6f94fd3f97fd91cafb667b5
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-21 20:46:30 +0700

    upload

--- api/user/uploadPicture.php
+++ api/user/uploadPicture.php
@@ -7,25 +7,21 @@ if(isset($_FILES["foto"]["type"]))
     $temporary = explode(".", $_FILES["foto"]["name"]);
     $file_extension = end($temporary);
     
-    if ((($_FILES["foto"]["type"] == "image/png") || ($_FILES["foto"]["type"] == "image/jpg") || ($_FILES["foto"]["type"] == "image/jpeg")) && ($_FILES["foto"]["size"] < 100000) ) {
-        if ($_FILES["foto"]["error"] > 0)
-        {
-            echo "Return Code: " . $_FILES["foto"]["error"] . "<br/><br/>";
-        }
-        else
-        {
-           
-                $sourcePath = $_FILES['foto']['tmp_name']; // Storing source path of the file in a variable
-                $filename=$_FILES["foto"]["name"];
-                $extension=end(explode(".", $filename));
-                $newfilename=$id .".".$extension;
+    if ($_FILES["foto"]["error"] > 0)
+    {
+        echo "Return Code: " . $_FILES["foto"]["error"] . "<br/><br/>";
+    }
+    else
+    {
+        
+            $sourcePath = $_FILES['foto']['tmp_name']; // Storing source path of the file in a variable
+            $filename=$_FILES["foto"]["name"];
+            $extension=end(explode(".", $filename));
+            $newfilename=$id .".".$extension;
 
-                $targetPath = "../upload/".$newfilename; // Target path where file is to be stored
-                move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
-                echo 'berhasil';
-        }
-    }else{
-        echo 'salah';
+            $targetPath = "../../upload/".$newfilename; // Target path where file is to be stored
+            move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
+            echo 'berhasil';
     }
 }else{
     echo 'gagal';
