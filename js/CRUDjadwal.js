c6943c0e00a056b4d1d5236703567471ca4c9532
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-21 10:46:03 +0700

    kalender

--- js/CRUDjadwal.js
+++ js/CRUDjadwal.js
@@ -89,24 +89,22 @@ function getUpdateKalender(id){
                             obj['eventsData'].push({"title":dtjdwl.jadwal,"start":jdwl.tanggal+'T'+dtjdwl.waktu});
                         });
                         console.log(obj.eventsData);
-                        
+                        $('#calendarData').fullCalendar({
+                            header: {
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'month,agendaWeek,agendaDay,listWeek'
+                            },
+                            editable: true,
+                            eventLimit: true, // allow "more" link when too many events
+                            navLinks: true,
+                            events: obj.eventsData
+                        });
                     });
                     console.log(obj.eventsData);
-
-                    $('#calendarData').fullCalendar({
-                        header: {
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'month,agendaWeek,agendaDay,listWeek'
-                        },
-                        editable: true,
-                        eventLimit: true, // allow "more" link when too many events
-                        navLinks: true,
-                        events: obj.eventsData
-                    });
-
             });
         });
+        console.log(obj.eventsData);
 }
 function getPrioritas(id){
     var jadwalPrioritas = "";
