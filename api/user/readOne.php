397f9acc19e84f25cb964e5a4d9198ac477c36e2
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-25 15:59:15 +0700

    admin

--- api/user/readOne.php
+++ api/user/readOne.php
@@ -20,51 +20,36 @@ $jadwal->id = $user->id;
 // read the details of user to be edited
 $user->readOne();
 $stmtCatatan = $catatan->readOne();
-$numCatatan = $stmtCatatan->rowCount();
 $stmtJadwal = $jadwal->readOne();
-$numJadwal = $stmtJadwal->rowCount();
 
-// create array
-if($numCatatan>0){
- 
-    // products array
-    $catatan_arr=array();
-    $catatan_arr["catatan"]=array();
+// products array
+$catatan_arr=array();
+$catatan_arr["catatan"]=array();
 
-    while ($rowCatatan = $stmtCatatan->fetch(PDO::FETCH_ASSOC)){
+while ($rowCatatan = $stmtCatatan->fetch(PDO::FETCH_ASSOC)){
 
-        extract($rowCatatan);
-        $product_item=array(
-            "catatan" => $catatan,
-         );
-         
-        array_push($catatan_arr["catatan"], $product_item);
-    }
+    extract($rowCatatan);
+    $product_item=array(
+        "catatan" => $catatan,
+        );
+        
+    array_push($catatan_arr["catatan"], $product_item);
 }
-else{
-    $catatan_arr["catatan"]=null;
-}
-if($numJadwal>0){
- 
-    // products array
-    $jadwal_arr=array();
-    $jadwal_arr["jadwal"]=array();
 
-    while ($rowJadwal = $stmtJadwal->fetch(PDO::FETCH_ASSOC)){
+$jadwal_arr=array();
+$jadwal_arr["jadwal"]=array();
 
-        extract($rowJadwal);
-        $product_item=array(
-            "jadwal" => $jadwal,
-            "waktu" => $waktu,
-            "tanggal" => $tanggal,
-            "tempat" => $tempat,
-         );
-         
-        array_push($jadwal_arr["jadwal"], $product_item);
-    }
-}
-else{
-    $jadwal_arr["jadwal"]=null;
+while ($rowJadwal = $stmtJadwal->fetch(PDO::FETCH_ASSOC)){
+
+    extract($rowJadwal);
+    $product_item=array(
+        "jadwal" => $jadwal,
+        "waktu" => $waktu,
+        "tanggal" => $tanggal,
+        "tempat" => $tempat,
+        );
+        
+    array_push($jadwal_arr["jadwal"], $product_item);
 }
 
 $user_arr = array(
