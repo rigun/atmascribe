4a641a0c5baeb056ac565a63b2d9dd67f64a83ea
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-09-27 14:49:30 +0700

    data

--- js/CRUDadmin.js
+++ js/CRUDadmin.js
@@ -283,8 +283,7 @@ function downloadPDF(){
     });
     $.getJSON("https://atmascribe.thekingcorp.org/api/user/read.php", function(data){
         $.each(data.records, function(key, val){
-            console.log('#pdf'+key);
-            doc.fromHTML($('#pdf'+key), 15, 15, {
+            doc.fromHTML($('#pdf'+key).html(), 15, 15, {
                 'width': 170,
                     'elementHandlers': specialElementHandlers
             });
